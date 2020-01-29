d42c30963a0cec80fa83d0d4d87da4848a5487af
Mike North <michael.l.north@gmail.com>
2020-01-28 23:44:14 -0800

    version bump

--- scripts/ver.sh
+++ scripts/ver.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 lerna version prerelease --preid dev --no-commit-hooks --no-push --no-git-tag-version && \
 git add -A && \
+git commit -m "version bump" --no-verify && \
 lerna run changelog && \
-git commit -m "version bump" --no-verify
+git commit --amend -m "version bump" && \
 lerna publish from-package
